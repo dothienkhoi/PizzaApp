709f1ba1592e1e96cad41533e08f8d0578a2a05a
dothienkhoi <dothienkhoi22022005@gmail.com>
2025-04-17 18:40:45 +0700

    Add project files.

---
+++ PizzaApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace PizzaApp.Pages;
+
+public partial class MainPage : ContentPage
+{
+    //int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private async void TapGestureRecogbizer_Tapped(System.Object sender,
+        Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+        await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+    }
+
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //    count++;
+
+    //    if (count == 1)
+    //        CounterBtn.Text = $"Clicked {count} time";
+    //    else
+    //        CounterBtn.Text = $"Clicked {count} times";
+
+    //    SemanticScreenReader.Announce(CounterBtn.Text);
+    //}
+}
